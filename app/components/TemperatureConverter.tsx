3d30d615bd01522f873b81470c68931efd253760
TN <naoufa.t@hotmail.com>
2023-11-15 01:53:05 +0100

    update code

--- app/components/TemperatureConverter.tsx
+++ app/components/TemperatureConverter.tsx
@@ -1,7 +1,44 @@
+"use client";
+import { useState } from "react";
+
 export default function TemperatureConverter() {
+  const [celsius, setCelsius] = useState();
+  const [fahrenheit, setFahrenheit] = useState();
+  const [kelvin, setKelvin] = useState();
+
+  function convertTemperature() {}
   return (
-    <div>
-      <p>Temperature Converter</p>
-    </div>
+    <>
+      <div className="flex flex-col gap-10 border-4 border-slate-500 p-10">
+        <div className="w-96 justify-between flex">
+          <label className="text-lg font-bold">Celsius °C:</label>
+          <input
+            type="number"
+            placeholder="Celsius"
+            className="text-lg text-center"
+            onChange={convertTemperature}
+          />
+        </div>
+
+        <div className="bg-red-500 h-1"></div>
+        <div className="w-96 justify-between flex">
+          <label className="text-lg font-bold">Fahrenheit °F:</label>
+          <input
+            type="number"
+            placeholder="Fahrenheit"
+            className="text-lg text-center"
+          />
+        </div>
+        <div className="bg-red-500 h-1"></div>
+        <div className="w-96 justify-between flex">
+          <label className="text-lg font-bold">Kelvin °K:</label>
+          <input
+            type="number"
+            placeholder="Kelvin"
+            className="text-lg text-center"
+          />
+        </div>
+      </div>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import TemperatureConverter from "./components/TemperatureConverter";
+import LINK from "next/link";
+import IMAGE from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <TemperatureConverter />
+        <div className="flex gap-14">
+          <LINK href="/">
+            <IMAGE src="/logo.png" alt="logo" width={100} height={100} />
+          </LINK>
+          <p className="text-6xl font-bold  text-slate-600 hover:text-slate-700 border-b-4 border-orange-600">
+            Temperature Converter
+          </p>
+          <LINK href="/">
+            <IMAGE src="/logo.png" alt="logo" width={100} height={100} />
+          </LINK>
+        </div>
+        <div className="flex p-14 justify-center">
+          <TemperatureConverter />
+        </div>
       </div>
     </main>
   );
